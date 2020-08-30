2cd28b568383c21d45c35d47d508802ad66efbec
Debid Magar <linkwithdm@gmail.com>
2020-08-30 17:43:03 +0545

    port change

--- app.js
+++ app.js
@@ -24,7 +24,10 @@ app.use("/user", userRouter);
 app.use("/question", questionRouter);
 
 //port listening
-app.listen(app.get("port"), function (err, done) {
+
+var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
+var server_host = process.env.YOUR_HOST || "0.0.0.0";
+app.listen(server_port, server_host, (err, done) => {
   if (err) {
     console.log("Error while listening port " + app.get("port") + " >> " + err);
   } else {
